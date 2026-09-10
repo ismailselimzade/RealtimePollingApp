889839a4c92936a41519b3c0d79f3e196038c509
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-09-10 11:33:42 +0400

    feat(client): implement WebSocket client with send/receive loop

--- RealtimePollingApp.Client/Program.cs
+++ RealtimePollingApp.Client/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net.WebSockets;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var client = new ClientWebSocket();
+
+await client.ConnectAsync(new Uri("wss://localhost:7062/ws"), CancellationToken.None);
+
+string message = "Client ?: ";
+
+await client.SendAsync(Encoding.UTF8.GetBytes(message), WebSocketMessageType.Text, true, CancellationToken.None);
+
+byte[] buffer = new byte[1024];
+
+while (client.State == WebSocketState.Open)
+{
+    var receiveResult = await client.ReceiveAsync(buffer, CancellationToken.None);
+    Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, receiveResult.Count));
+}
